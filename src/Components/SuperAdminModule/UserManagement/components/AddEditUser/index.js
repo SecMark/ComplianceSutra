974a3bc370fc39563164fb3a79e5b4660a115aa8
ashukumar2021 <86044488+ashukumar2021@users.noreply.github.com>
2021-09-01 19:45:22 +0530

    all 3 steps done

--- src/Components/SuperAdminModule/UserManagement/components/AddEditUser/index.js
+++ src/Components/SuperAdminModule/UserManagement/components/AddEditUser/index.js
@@ -1,34 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useHistory, withRouter } from "react-router";
 import { AiOutlineClose } from "react-icons/ai";
-import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import "./style.css";
 import StepperProgressIcon from "../../../../../assets/Icons/stepper-progress-cricle.svg";
 import StepperCheckIcon from "../../../../../assets/Icons/sidebarCheckIcon.png";
 import StepperCheckIconActive from "../../../../../assets/Icons/check.png";
-
-const roles = [
-  {
-    value: "team-member",
-    lable: "Team Member",
-  },
-  {
-    value: "super-admin",
-    lable: "Super Admin",
-  },
-];
 const AddEditUser = (props) => {
   const [stepper, setStepper] = useState({
     stepperAcitveSlide: 1,
     stepperCompletedSlides: [],
   });
-  // const handleAddNewRoleClick = () => {
-  //   if (currentSelectedRole !== "") {
-  //     selectedRoles([...selectedRoles, currentSelectedRole]);
-  //     setCurrentSelectedRole("");
-  //   }
-  // };
   const handleStepClick = (step) => {
     const completedSlides = stepper.stepperCompletedSlides;
     if (completedSlides.includes(step)) {
@@ -39,9 +21,6 @@ const AddEditUser = (props) => {
       });
     }
   };
-  useEffect(() => {
-    console.log(stepper);
-  }, [stepper]);
   return (
     <div className="spacing">
       <div className="row">
@@ -227,6 +206,17 @@ const AddEditUser = (props) => {
               <option>1 year</option>
             </select>
           </div>
+          <div className="col-md-6">
+            <label
+              htmlFor="certificate-validity"
+              className="form-control-label"
+            >
+              Date of Validity
+            </label>
+            <select id="certificate-validity" className="form-control">
+              <option>1 year</option>
+            </select>
+          </div>
           <div className="col-md-12">
             <button className="stroke-button my-4 d-block">
               add new cirtification
@@ -250,31 +240,178 @@ const AddEditUser = (props) => {
         </div>
       )}
       {stepper.stepperAcitveSlide === 2 && (
-        <div>
-          <h2>Slide 2</h2>
-          <button
-            className="primary-button mt-3 d-block"
-            onClick={() =>
-              setStepper({
-                ...stepper,
-                stepperAcitveSlide: 3,
-                stepperCompletedSlides: [...stepper.stepperCompletedSlides, 2],
-              })
-            }
-          >
-            Next
-          </button>
+        <div className="row">
+          <div className="col-12">
+            <h6>Reviewer's Expertise Details</h6>
+            <div className="col-md-6 mx-0 px-0">
+              <label
+                htmlFor="license-expertise"
+                className="form-control-label mt-2"
+              >
+                License Expertise
+              </label>
+              <select id="license-expertise" className="form-control px-0">
+                <option disabled selected>
+                  Select a license
+                </option>
+                <option>NSE</option>
+              </select>
+              <button className="stroke-button mt-3 px-0">
+                add new license
+              </button>
+            </div>
+          </div>
+          <div className="col-12 mt-5">
+            <h6>Associated Client Details</h6>
+            <div className="col-md-6 mx-0 px-0">
+              <label
+                htmlFor="license-expertise"
+                className="form-control-label mt-2"
+              >
+                Add a client
+              </label>
+              <select id="license-expertise" className="form-control px-0">
+                <option disabled selected>
+                  Select client to allocate
+                </option>
+                <option>NSE</option>
+              </select>
+              <button className="stroke-button mt-3 px-0">
+                add a new client
+              </button>
+            </div>
+          </div>
+          <div className="col-12 mt-5">
+            <button className="primary-button primary-button--outlined mr-3">
+              go back
+            </button>
+            <button
+              className="primary-button"
+              onClick={() =>
+                setStepper({
+                  ...stepper,
+                  stepperAcitveSlide: 3,
+                  stepperCompletedSlides: [
+                    ...stepper.stepperCompletedSlides,
+                    2,
+                  ],
+                })
+              }
+            >
+              Next
+            </button>
+          </div>
         </div>
       )}
       {stepper.stepperAcitveSlide === 3 && (
-        <div>
-          <h2>Slide 3</h2>
-          {/* <button
-            className="primary-button mt-3 d-block"
-            onClick={() => setStepper({ ...stepper, stepperAcitveSlide: 3 })}
-          >
-            Next
-          </button> */}
+        <div class="row">
+          <div className="col-12 mb-3">
+            <div className="d-flex align-items-center">
+              <h5 className="mb-0">Personal Details</h5>
+              <button className="stroke-button ml-auto">edit</button>
+            </div>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Team member's Name
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">Krishna Kumar</p>
+          </div>
+
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Mobile Number
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">9599182599</p>
+          </div>
+
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Email ID
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">krishna.k@secmark.in</p>
+          </div>
+
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Address
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">120201, Surat, Gujarat</p>
+          </div>
+          <div className="col-12 my-3">
+            <div className="d-flex align-items-center">
+              <h5 className="mb-0">Internal Role Details</h5>
+              <button className="stroke-button ml-auto">edit</button>
+            </div>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Role 1
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">Content Manager</p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Role 2
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">Expert Reviwer</p>
+          </div>
+          <div className="col-12 my-3">
+            <div className="d-flex align-items-center">
+              <h5 className="mb-0">Certification Details</h5>
+              <button className="stroke-button ml-auto">edit</button>
+            </div>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Certificate 1
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">
+              GST Validation (valid till 12 Mar, 2021)
+            </p>
+          </div>
+          <div className="col-12 my-3">
+            <div className="d-flex align-items-center">
+              <h5 className="mb-0">Reviewer's Expertise Details</h5>
+              <button className="stroke-button ml-auto">edit</button>
+            </div>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text review-details__text--label">
+              Licenses
+            </p>
+          </div>
+          <div className="col-6">
+            <p className="review-details__text">
+              Good and Service tax (GST)
+              <br />
+              National Stock Exchange (NSE)
+              <br />
+              Bombay Stock Exchange (BSE)
+            </p>
+          </div>
+          <div className="col-12">
+            <button
+              className="primary-button mt-3 d-block"
+              onClick={() => setStepper({ ...stepper, stepperAcitveSlide: 3 })}
+            >
+              confirm & add member
+            </button>
+          </div>
         </div>
       )}
     </div>
