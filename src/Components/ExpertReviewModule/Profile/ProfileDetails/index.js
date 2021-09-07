4d454b426c4162a4a8e7312405289fdcf8c84551
amit-trakiot <88842861+amit-trakiot@users.noreply.github.com>
2021-09-07 09:57:58 +0530

    CSUT-76

--- src/Components/ExpertReviewModule/Profile/ProfileDetails/index.js
+++ src/Components/ExpertReviewModule/Profile/ProfileDetails/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./style.css";
 import api from "../../../../apiServices";
+import { actions as coActions } from "../../../OnBording/SubModules/DashBoardCO/redux/actions";
 import Select, { components } from "react-select";
 import { IoAddCircle } from "react-icons/io5";
 import { MdLock } from "react-icons/md";
@@ -9,15 +10,30 @@ import { MdLock } from "react-icons/md";
 const ProfileDetails = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
+  const [valuesBackup, setValuesBackup] = useState(null);
   const [email, setEmail] = useState(null);
   const [number, setNumber] = useState(null);
+  const [userInfoBackup, setUserInfoBackup] = useState(null);
+  const [valuesChanged, setValuesChanged] = useState(false);
   const [values, setValues] = useState({
     userName: "",
     designation: "",
     emailId: "",
     mobileNo: "",
-    countrycode: "",
   });
+
+  const [LicenceValue, setLicenceValue] = useState([]);
+
+  const loggedUser = state && state.auth && state.auth.loginInfo;
+
+  useEffect(() => {
+    dispatch(
+      coActions.availabilityCheckequest({
+        loginID: loggedUser.EmailID,
+        loginty: "AdminEmail",
+      })
+    );
+  }, []);
   useEffect(() => {
     const _name =
       state &&
@@ -55,12 +71,9 @@ const ProfileDetails = () => {
         _designation != "" && _designation != undefined ? _designation : "",
       emailId: _emailId != "" && _emailId != undefined ? _emailId : "",
       mobileNo: _mobile != "" && _mobile != undefined ? Number(_mobile) : "",
-      countrycode:
-        _userInfo !== "" && _userInfo !== undefined
-          ? _userInfo.countrycode
-          : "",
     };
-
+    setValuesBackup(userObj);
+    setUserInfoBackup(_userInfo);
     setValues(userObj);
   }, [state.taskReport.userAvailability]);
   const LicenseOptions = [
@@ -82,6 +95,53 @@ const ProfileDetails = () => {
       label: "Non-commercial services",
     },
   ];
+
+  const onChangeHandler = (name) => (event) => {
+    if (name === "userName" || name === "designation") {
+      const re = /^[a-z|A-Z_ ]*$/;
+      if (event.target.value && !re.test(event.target.value)) {
+        return "";
+      }
+    }
+    if (name === "mobileNo") {
+      const mobileNumberReg = /^[0-9]{0,10}$/;
+      if (!mobileNumberReg.test(Number(event.target.value))) {
+        return "";
+      }
+      setNumber(event.target.value);
+    }
+    if (name === "emailId") {
+      setEmail(event.target.value);
+    }
+    setValuesChanged(true);
+    setValues({ ...values, [name]: event.target.value });
+
+    // }
+  };
+
+  const onDropDownChnage = (selectedOptions) => {
+    let licarray = [];
+    selectedOptions.forEach((element) => {
+      licarray.push(element.value);
+    });
+    setLicenceValue(licarray);
+    setValuesChanged(true);
+  };
+
+  const onSubmit = () => {
+    let payload = {
+      adminName: values.userName,
+      adminMobile: values.mobileNo,
+      adminEmail: values.emailId,
+      userType: values.emailId !== valuesBackup.emailId ? 9 : 1,
+      actionFlag: 2,
+      designation: values.designation,
+      userID: userInfoBackup.UserID,
+      // lic_expertee: LicenceValue,
+    };
+    dispatch(coActions.coDetailsInsUpdDelRequest(payload));
+    setValuesChanged(false);
+  };
 
   const customStyles = {
     multiValue: () => ({
@@ -168,6 +228,7 @@ const ProfileDetails = () => {
               type="text"
               defaultValue="rameshkumar@secmark.co.in"
               value={values.emailId}
+              onChange={onChangeHandler("emailId")}
               disabled
             />
             <MdLock
@@ -184,6 +245,7 @@ const ProfileDetails = () => {
               type="text"
               defaultValue="Ramesh Kumar"
               value={values.userName}
+              onChange={onChangeHandler("userName")}
             ></input>
           </div>
           <div className="FormElement">
@@ -192,6 +254,7 @@ const ProfileDetails = () => {
               type="text"
               defaultValue="+91  9434721588"
               value={values.mobileNo}
+              onChange={onChangeHandler("mobileNo")}
             />
           </div>
           <div className="FormElement">
@@ -200,6 +263,7 @@ const ProfileDetails = () => {
               type="text"
               defaultValue="Expert Reviewer"
               value={values.designation}
+              onChange={onChangeHandler("designation")}
             />
           </div>
         </form>
@@ -211,9 +275,10 @@ const ProfileDetails = () => {
             styles={customStyles}
             defaultValue={LicenseOptions[0]}
             options={LicenseOptions || []}
+            onChange={onDropDownChnage}
           />
         </div>
-        <div className="FormElement">
+        {/* <div className="FormElement">
           <p>Industry Expert in:</p>
           <Select
             components={{ DropdownIndicator }}
@@ -222,10 +287,22 @@ const ProfileDetails = () => {
             defaultValue={IndustryOptions[0]}
             options={IndustryOptions || []}
           />
-        </div>
+        </div> */}
         <div className="ProfileSubmit">
-          <button>SAVE CHANGES</button>
-          <button id="Discard">DISCARD CHANGES</button>
+          <button disabled={valuesChanged === false} onClick={() => onSubmit()}>
+            SAVE CHANGES
+          </button>
+          {valuesChanged && (
+            <button
+              id="Discard"
+              onClick={() => {
+                setValues(valuesBackup);
+                setValuesChanged(false);
+              }}
+            >
+              DISCARD CHANGES
+            </button>
+          )}
         </div>
       </div>
     </div>
