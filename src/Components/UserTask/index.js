6afbd914ac5e21addaeb4b2e42a5bd0de32c6506
Nirbyoleek Das <nirbyoleekd@trakiot.in>
2021-10-11 19:39:37 +0530

    Fixes

--- src/Components/UserTask/index.js
+++ src/Components/UserTask/index.js
@@ -11,43 +11,30 @@ const UserTaskDetails = () => {
         <h4>User Task</h4>
 
         <div className="UserTaskType">
-          <div>All Tasks</div>
+          <div className="UserTypeSelected">All Tasks</div>
           <div>Current Tasks</div>
           <div>Pending Tasks</div>
           <div>Rejected Tasks</div>
         </div>
 
-        <div id="Line1"></div>
+        {/* <div id="Line1"></div> */}
         <div className="UserHeader">
-          <div
-            style={{
-              width: "150px",
-            }}
-          >
-            Project Name
-          </div>
-          <div style={{ width: "140px" }}>Task</div>
-          <div style={{ width: "70px" }}>Schedule Date</div>
-          <div style={{ width: "70px" }}>Owner Name</div>
-          <div style={{ width: "70px" }}>Start Date</div>
-          <div style={{ width: "70px" }}>End Date</div>
-          <div style={{ width: "70px" }}>Duration Time</div>
-          <div style={{ width: "80px" }}>Migrated On </div>
-          <div style={{ width: "60px" }}>Task Status</div>
-          <div style={{ width: "100px" }}>Completion (Percentage)</div>
-          <div
-            style={{
-              width: "60px",
-              marginLeft: "20px",
-            }}
-          >
-            Action
-          </div>
+          <div className="UserProject">Project Name</div>
+          <div className="UserTypeTask">Task</div>
+          <div className="UserTaskElements">Scheduled Date</div>
+          <div className="UserTaskElements">Owner Name</div>
+          <div className="UserTaskElements">Start Date</div>
+          <div className="UserTaskElements">End Date</div>
+          <div className="UserTaskElements">Duration Time</div>
+          <div className="UserTaskElements">Migrated On </div>
+          <div className="UserTaskElements">Task Status</div>
+          <div className="UserTaskComplete">Completion (Percentage)</div>
+          <div className="UserActions">Action</div>
         </div>
 
         <div className="UserElement">
-          <div style={{ maxWidth: "150px" }}>Check the Project </div>
-          <div style={{ maxWidth: "140px" }}>Check Hero section</div>
+          <div className="UserProject">Check the Project </div>
+          <div className="UserTask">Check Hero section</div>
           <div>18th Sep 2021</div>
           <div>Ajit</div>
           <div>21th Sep 2021</div>
@@ -55,14 +42,14 @@ const UserTaskDetails = () => {
           <div>4 Days</div>
           <div>27th Sep 2021</div>
           <div>Completed</div>
-          <div>100%</div>
+          <div className="CompleteTask">100%</div>
           <div className="UserActions">
             <img src={UserComplete}></img>
             <img src={UserText}></img>
           </div>
         </div>
         <div className="UserElement">
-          <div style={{ maxWidth: "150px" }}>Check the Project </div>
+          <div className="UserProject">Check the Project </div>
           <div style={{ maxWidth: "140px" }}>Check Hero section</div>
           <div>18th Sep 2021</div>
           <div>Ajit</div>
@@ -71,15 +58,15 @@ const UserTaskDetails = () => {
           <div>4 Days</div>
           <div>27th Sep 2021</div>
           <div>Completed</div>
-          <div>100%</div>
+          <div className="CompleteTask">100%</div>
           <div className="UserActions">
             <img src={UserComplete}></img>
             <img src={UserText}></img>
           </div>
         </div>
         <div className="UserElement">
-          <div style={{ maxWidth: "150px" }}>Check the Project </div>
-          <div style={{ maxWidth: "140px" }}>Check Hero section</div>
+          <div className="UserProject">Check the Project </div>
+          <div className="UserTask">Check Hero section</div>
           <div>18th Sep 2021</div>
           <div>Ajit</div>
           <div>21th Sep 2021</div>
@@ -87,15 +74,15 @@ const UserTaskDetails = () => {
           <div>4 Days</div>
           <div>27th Sep 2021</div>
           <div>Completed</div>
-          <div>100%</div>
+          <div className="CompleteTask">100%</div>
           <div className="UserActions">
             <img src={UserComplete}></img>
             <img src={UserText}></img>
           </div>
         </div>
         <div className="UserElement">
-          <div style={{ maxWidth: "150px" }}>Check the Project </div>
-          <div style={{ maxWidth: "140px" }}>Check Hero section</div>
+          <div className="UserProject">Check the Project </div>
+          <div className="UserTask">Check Hero section</div>
           <div>18th Sep 2021</div>
           <div>Ajit</div>
           <div>21th Sep 2021</div>
@@ -103,7 +90,7 @@ const UserTaskDetails = () => {
           <div>4 Days</div>
           <div>27th Sep 2021</div>
           <div>Completed</div>
-          <div>100%</div>
+          <div className="CompleteTask">100%</div>
           <div className="UserActions">
             <img src={UserComplete}></img>
             <img src={UserText}></img>
