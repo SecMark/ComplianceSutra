76fcd95e16e158b0858300c260671405f5ae085c
amit-trakiot <88842861+amit-trakiot@users.noreply.github.com>
2021-10-14 09:53:04 +0530

    CSUT-334 Add Project Mobile View

--- src/Components/ExpertReviewModule/AddandEditProject/index.js
+++ src/Components/ExpertReviewModule/AddandEditProject/index.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddProject from "./AddProjectModal";
+import AddProjectMobile from "./AddProjectMobile";
 
 function AddNewProject() {
   const [show, setShow] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
   return (
     <div>
       <button onClick={() => setShow(true)}>Add Project</button>
-      <AddProject onClose={()=>setShow(false)} show={show} />
+
+      {width <= 768 ? (
+        <AddProjectMobile />
+      ) : (
+        <AddProject onClose={() => setShow(false)} show={show} />
+      )}
     </div>
   );
 }
