ba273d6d6c0960cc6ad583ba73be67787272dca5
amit-trakiot <88842861+amit-trakiot@users.noreply.github.com>
2021-11-10 17:51:50 +0530

    added the invite link api

--- src/Components/ComplianceModule/ComplianceDemo7/index.js
+++ src/Components/ComplianceModule/ComplianceDemo7/index.js
@@ -1,21 +1,136 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 import leftTopCorner from "../../../assets/Images/Compliancedemo/left-top-corner.png";
 import Calendar from "../../../assets/Images/Compliancedemo/calnderLast.png";
+import { useDispatch, useSelector } from "react-redux";
+import { actions as emailActions } from "../../OnBording/redux/actions";
 import Leaderboard from "../../../assets/Images/Compliancedemo/leaderboard.png";
 import Teamperformance from "../../../assets/Images/Compliancedemo/team-performance.png";
+import BackDrop from "../../../CommonModules/sharedComponents/Loader/BackDrop";
+import { toast } from "react-toastify";
 import DemoEnd from "../DemoNav";
+import { isEmail } from "./utils";
+import axiosInstance from "../../../apiServices";
 
 function ComplianceDemo7() {
-   let imga = leftTopCorner
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const [inputBorder, setInputBorder] = useState(false);
+  const [isValidate, setIsValidate] = useState(false);
+  const [isEmailExist, setIsEmailExist] = useState(false);
+  const [show, setShow] = useState(false);
+  const [checkBoxState, setCheckBoxState] = useState(false);
+  const [values, setValues] = useState({
+    loginID: "",
+    loginty: "AdminEmail",
+  });
+
+  let emailAlreadExist =
+    state &&
+    state.complianceOfficer &&
+    state.complianceOfficer.emailAlreadExist;
+  let isLoading =
+    state && state?.complianceOfficer && state?.complianceOfficer.loader;
+  useEffect(() => {
+    dispatch(emailActions.setLoader(false));
+  }, []);
+  useEffect(() => {
+    console.log({ emailAlreadExist, isEmail: isEmail(values.loginID) });
+    if (isEmail(values.loginID))
+      if (
+        emailAlreadExist === false &&
+        emailAlreadExist !== "" &&
+        isEmail(values.loginID)
+      ) {
+        setIsEmailExist(false);
+      } else if (
+        emailAlreadExist === true &&
+        emailAlreadExist !== "" &&
+        isEmail(values.loginID)
+      ) {
+        setIsEmailExist(true);
+      }
+  }, [emailAlreadExist, values.loginID]);
+
+  const onChangeHandler = (name) => (event) => {
+    if (name === "loginID" && isEmail(event.target.value)) {
+      axiosInstance
+        .post("compliance.api.avabilityCheck", { email: event.target.value })
+        .then((response) => {
+          if (
+            response &&
+            response.data &&
+            response.data.message &&
+            response.data.message.status === true
+          ) {
+            dispatch(
+              emailActions.verifyEmailRequestFailed({
+                verifyEmail: false,
+                emailAlreadyExistMessage: true,
+                email: event.target.value,
+              })
+            );
+          } else if (
+            response &&
+            response.data &&
+            response.data.message &&
+            response.data.message.status === false
+          ) {
+            dispatch(
+              emailActions.verifyEmailRequestFailed({
+                verifyEmail: false,
+                emailAlreadyExistMessage: false,
+                email: event.target.value,
+              })
+            );
+          }
+        });
+    } else if (name === "loginID" && !isEmail(event.target.value)) {
+      setIsEmailExist(false);
+    }
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const onSubmit = () => {
+    setIsValidate(true);
+    if (!isEmail(values.loginID) || values.loginID === "") {
+      return;
+    }
+    setIsValidate(false);
+    setIsEmailExist(false);
+    if (checkBoxState === true) {
+      dispatch(
+        emailActions.verifyEmailRequest({
+          email: values.loginID,
+        })
+      );
+      dispatch(emailActions.setLoader(true));
+      setTimeout(() => {
+        let status = state.complianceOfficer.isVerifiedEmail;
+      }, [100]);
+    } else if (!checkBoxState) {
+      toast.error("Please accept the terms and conditions to go ahead");
+    }
+  };
+  const onCheckboxChange = (e) => {
+    setCheckBoxState(e.target.checked);
+  };
+
   return (
     <div>
-       <div>
-      <DemoEnd />
+      <div>
+        <DemoEnd />
       </div>
       <div className="compliance-end">
-        <div className="left-top-corner" style={{backgroundImage:`url(${imga})`,objectFit:"cover"}}>
+        <BackDrop isLoading={isLoading} />
+        <div
+          className="left-top-corner"
+          style={{
+            backgroundImage: `url(${leftTopCorner})`,
+            objectFit: "cover",
+          }}
+        >
           {/* <img src={leftTopCorner} alt="left-top-corner" /> */}
         </div>
         <div className="back-ground-image">
@@ -53,12 +168,53 @@ function ComplianceDemo7() {
                         type="email"
                         class="form-control"
                         placeholder="Enter email"
+                        onChange={onChangeHandler("loginID")}
                       />
+
                       <span class="input-group-btn">
-                        <button class="btn invite-btn" type="submit">
+                        <button
+                          class="btn invite-btn"
+                          type="submit"
+                          onClick={() => onSubmit()}
+                          disabled={
+                            isEmailExist ||
+                            isValidate ||
+                            !isEmail(values.loginID)
+                          }
+                        >
                           INVITE
                         </button>
                       </span>
+                    </div>
+                    {isValidate && values.loginID === "" && (
+                      <p className="input-error-message">Email is required</p>
+                    )}
+                    {values.loginID !== "" && !isEmail(values.loginID) && (
+                      <p className="input-error-message">
+                        Please enter valid email address
+                      </p>
+                    )}
+                    {isEmailExist && isEmailExist === true && (
+                      <p className="input-error-message">
+                        Email already exists please login
+                      </p>
+                    )}
+                    <div className="custom-control custom-checkbox">
+                      <input
+                        id="magicBtn"
+                        type="checkbox"
+                        className="custom-control-input"
+                        style={{ width: "1rem", height: "1.25rem" }}
+                        onChange={(e) => onCheckboxChange(e)}
+                        checked={checkBoxState}
+                      />
+
+                      <label className="custom-control-label" for="magicBtn">
+                        I agree to all the
+                      </label>
+                      <button className="Terms" onClick={() => setShow(true)}>
+                        Terms & Conditions
+                      </button>
                     </div>
                   </div>
                 </div>
