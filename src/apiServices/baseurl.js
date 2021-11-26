7cf6772e2c2fc63974474d903bf28c7f5214bbf4
jatin-dev <86044141+jatin-trakiot@users.noreply.github.com>
2021-11-26 11:07:17 +0530

    Update baseurl.js

--- src/apiServices/baseurl.js
+++ src/apiServices/baseurl.js
@@ -2,8 +2,6 @@ let url = "";
 
 if (window.location.href.includes("localhost")) {
   url = "https://dev.compliancesutra.com/api/method/";
-} else if (window.location.href.includes("http://52.172.231.19/")) {
-  url = "https://dev.compliancesutra.com/api/method/";
 } else {
   url = "https://dev.compliancesutra.com/api/method/";
 }
