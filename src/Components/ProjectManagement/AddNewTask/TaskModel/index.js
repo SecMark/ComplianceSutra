19a191123cb75c9c87268253380b5d11716b3c35
amit-trakiot <88842861+amit-trakiot@users.noreply.github.com>
2021-11-29 12:10:56 +0530

    adding new task ui

--- src/Components/ProjectManagement/AddNewTask/TaskModel/index.js
+++ src/Components/ProjectManagement/AddNewTask/TaskModel/index.js
@@ -9,7 +9,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-
+import axiosInstance from "../../../../apiServices";
 // custom style for dropdown
 const customStyle = {
   control: (styles) => ({
@@ -21,7 +21,17 @@ const customStyle = {
 };
 
 function NewTaskModel({ showTask, onClose }) {
+  const [frequency, setFrequency] = useState("None");
+  const [userLilst, setUserList] = useState([]);
+  const [values,setValues] =useState({
+    assign_user:[]
+  })
+  const [weeklyDay, setWeeklyDay] = useState("Monday");
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  console.log(acceptedFiles);
+  console.log(frequency);
+  console.log(weeklyDay);
   const calanderimg = <img src={calanderIcon} />;
 
   const files = acceptedFiles.map((file) => (
@@ -30,6 +40,48 @@ function NewTaskModel({ showTask, onClose }) {
     </li>
   ));
 
+ useEffect(()=>{
+  getRegisteredUSerList();
+ },[])
+
+  // function to get the registered user list
+  const getRegisteredUSerList = () => {
+    axiosInstance.get("compliance.api.getAllUsersList").then((response) => {
+      const arr1 = [];
+      response.data.message.user_list.map((el) => {
+        arr1.push({
+          label: el.name,
+          value: el.full_name,
+        });
+      });
+      setUserList(arr1);
+    });
+  };
+
+  // function to change dropdownvalue
+
+  const handleDropDownChange = (val) => {
+    const arr2 = [];
+    val.map((label) => {
+      arr2.push(label.label);
+    });
+    setValues({
+      ...values,
+      assign_user: arr2,
+    });
+  };
+  
+  const FrequencyChange = (e) => {
+    setFrequency(e.target.value);
+  };
+  const onDayChange = (val) => {
+    setWeeklyDay(val);
+  };
+
+
+  const onDateChange = (date, dateString, name) => {
+
+  }
   return !showTask ? null : (
     <div className="add-edit-modal" onClick={onClose}>
       <div
@@ -49,8 +101,8 @@ function NewTaskModel({ showTask, onClose }) {
               <CreatableSelect
                 isMulti
                 styles={customStyle}
-                // onChange={handleDropDownChange}
-                // options={userLilst}
+                onChange={handleDropDownChange}
+                options={userLilst}
               />
             </div>
             <div className="col-sm-6 col-lg-3">
@@ -120,58 +172,154 @@ function NewTaskModel({ showTask, onClose }) {
           </div>
           <div className="row">
             <div className="col">
-            <label className="add-edit-project-labels mt-3">Frequency</label>
-              <div className="add-new-task-radio-btn">
-              <RadioGroup
-                row
-                aria-label="frequency"
-                name="row-radio-buttons-group"
-              >
-                <FormControlLabel
-                  value="none"
-                  control={<Radio />}
-                  label="none"
-                />
-                <FormControlLabel
-                  value="daily"
-                  control={<Radio />}
-                  label="daily"
-                />
-                <FormControlLabel
-                  value="weekly"
-                  control={<Radio />}
-                  label="weekly"
-                />
-                <FormControlLabel
-                  value="weekly"
-                  control={<Radio />}
-                  label="weekly"
-                />
-                <FormControlLabel
-                  value="Monthly"
-                  control={<Radio />}
-                  label="Monthly"
-                />
-                <FormControlLabel
-                  value="Quarterly"
-                  control={<Radio />}
-                  label="Quarterly"
-                />
-                <FormControlLabel
-                  value="Half Yearly"
-                  control={<Radio />}
-                  label="Half Yearly"
-                />
-                 <FormControlLabel
-                  value="Yearly"
-                  control={<Radio />}
-                  label="Yearly"
-                />
-                
-              </RadioGroup>
+              <label className="add-edit-project-labels mt-3">Frequency</label>
+              <div className="add-new-task-radio-btn justify-content-center">
+                <RadioGroup
+                  row
+                  className="justify-content-center"
+                  aria-label="frequency"
+                  name="row-radio-buttons-group"
+                  value={frequency}
+                  onChange={FrequencyChange}
+                >
+                  <FormControlLabel
+                    value="None"
+                    control={<Radio />}
+                    label="None"
+                  />
+                  <FormControlLabel
+                    value="Daily"
+                    control={<Radio />}
+                    label="Daily"
+                  />
+                  <FormControlLabel
+                    value="Weekly"
+                    control={<Radio />}
+                    label="Weekly"
+                  />
+                  <FormControlLabel
+                    value="Monthly"
+                    control={<Radio />}
+                    label="Monthly"
+                  />
+                  <FormControlLabel
+                    value="Quarterly"
+                    control={<Radio />}
+                    label="Quarterly"
+                  />
+                  <FormControlLabel
+                    value="Half Yearly"
+                    control={<Radio />}
+                    label="Half Yearly"
+                  />
+                  <FormControlLabel
+                    value="Yearly"
+                    control={<Radio />}
+                    label="Yearly"
+                  />
+                </RadioGroup>
               </div>
             </div>
           </div>
+          {frequency === "Weekly" && (
+            <div className="row mt-3">
+              <div className="col">
+                <div className="add-new-task-radio-weekly-btn d-flex flex-wrap justify-content-center">
+                  <h1
+                    className={`${
+                      weeklyDay === "Monday" && "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Monday");
+                    }}
+                  >
+                    Monday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Tuesday" && "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Tuesday");
+                    }}
+                  >
+                    Tuesday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Wednesday" &&
+                      "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Wednesday");
+                    }}
+                  >
+                    Wednesday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Thursday" &&
+                      "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Thursday");
+                    }}
+                  >
+                    Thursday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Friday" && "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Friday");
+                    }}
+                  >
+                    Friday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Saturday" &&
+                      "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Saturday");
+                    }}
+                  >
+                    Saturday
+                  </h1>
+                  <h1
+                    className={`${
+                      weeklyDay === "Sunday" && "add-task-weekly-day-selection"
+                    }`}
+                    onClick={() => {
+                      onDayChange("Sunday");
+                    }}
+                  >
+                    Sunday
+                  </h1>
+                </div>
+              </div>
+            </div>
+          )}
+          {
+            frequency==="Monthly" &&
+            <div className="row mt-3">
+                <div className="col justify-content-center">
+                <DatePicker
+                className="add-edit-project-inputs"
+                suffixIcon={calanderimg}
+                // onChange={(date, dateString) => {
+                //   setValues({
+                //     ...values,
+                //     end_date: dateString,
+                //   });
+                // }}
+              />
+                </div>
+            </div>
+            
+          }
           <div className="d-flex mt-3 justify-content-center">
             <div className="p-2">
               <button className="add-edit-project-submit-btn" onClick={onClose}>
--- src/Components/ProjectManagement/AddandEditProject/AddProjectModal/index.js
+++ src/Components/ProjectManagement/AddandEditProject/AddProjectModal/index.js
@@ -8,13 +8,13 @@ import calanderIcon from "../../../../assets/Icons/calanderIcon.svg";
 import api from "../../../../apiServices";
 import axiosInstance from "../../../../apiServices";
 import CreatableSelect from "react-select/creatable";
-
+import moment from "moment";
 
 function AddProject({ show, onClose }) {
-  
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
   const [userLilst, setUserList] = useState([]);
+  const [validate, setIsValidate] = useState(true);
   const [values, setValues] = useState({
     project_name: "",
     assign_user: [],
@@ -22,68 +22,121 @@ function AddProject({ show, onClose }) {
     end_date: "",
     project_overview: "",
   });
+  const [errors, setErrors] = useState({
+    project_Err: "",
+    assign_Err: "",
+    start_date_Err: "",
+    end_date_Err: "",
+    project_overview_Err: "",
+  });
   console.log("got this values", values);
   console.log("userList", userLilst);
   useEffect(() => {
     getRegisteredUSerList();
   }, []);
 
-// custom style for dropdown
-  const customStyle ={
+  // custom style for dropdown
+  const customStyle = {
     control: (styles) => ({
       ...styles,
       width: "100%",
       height: "50px",
-      borderRadius: "10px"
+      borderRadius: "10px",
     }),
-  }
-
+  };
 
   // function to get the registered user list
   const getRegisteredUSerList = () => {
     axiosInstance.get("compliance.api.getAllUsersList").then((response) => {
       const arr1 = [];
-      response.data.message.user_list.map((el)=>{
+      response.data.message.user_list.map((el) => {
         arr1.push({
-          label:el.name,
-          value:el.full_name
-        })
-      })
+          label: el.name,
+          value: el.full_name,
+        });
+      });
       setUserList(arr1);
     });
   };
 
   const onHandleChange = (evt) => {
     const value = evt.target.value;
-    console.log(evt);
     setValues({
       ...values,
       [evt.target.name]: value,
     });
   };
 
-
   // function to change dropdownvalue
- 
-  const handleDropDownChange = (val)=>{
-      const arr2 = []
-      val.map((label)=>{
-          arr2.push(label.label)
-      })
-      setValues({
-        ...values,
-        assign_user:arr2
-      })
 
+  const handleDropDownChange = (val) => {
+    const arr2 = [];
+    val.map((label) => {
+      arr2.push(label.label);
+    });
+    setValues({
+      ...values,
+      assign_user: arr2,
+    });
+  };
 
-  }
+  const EmptyfieldsCheck = () => {
+    if (
+      values.project_name === "" ||
+      values.assign_user.length === 0 ||
+      values.start_date === "" ||
+      values.end_date === "" ||
+      values.project_overview === ""
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   // submiting form values
-  const onSubmitValue = () =>{
+  const onSubmitValue = () => {
     const payload = values;
     dispatch(setProject(payload));
-    onClose()
-  }
+    onClose();
+  };
+
+  const onDateChange = (date, dateString, name) => {
+    const todayDate = moment().format("YYYY-MM-DD");
+    if (name === "start_date") {
+      setValues({
+        ...values,
+        start_date: dateString,
+      });
+      if (moment(todayDate).isAfter(dateString)) {
+        setErrors({
+          ...errors,
+          start_date_Err: `please select ${todayDate} or after ${todayDate}`,
+        });
+      } else {
+        setErrors({
+          ...errors,
+          start_date_Err: "",
+        });
+      }
+    } else if (name === "end_date") {
+      setValues({
+        ...values,
+        end_date: dateString,
+      });
+      if (moment(todayDate).isAfter(dateString)) {
+        setErrors({
+          ...errors,
+          end_date_Err: `please select ${todayDate} or after ${todayDate}`,
+        });
+      } else {
+        setErrors({
+          ...errors,
+          end_date_Err: "",
+        });
+      }
+    }
+  };
 
   const calanderimg = <img src={calanderIcon} />;
 
@@ -100,6 +153,10 @@ function AddProject({ show, onClose }) {
             name="project_name"
             onChange={onHandleChange}
           />
+          {values.project_name === "" && (
+            <p className="add-project-err-msg">Project Name can't be empty</p>
+          )}
+
           <div className="row mt-3">
             <div className="col-sm-12 col-lg-6">
               <label className="add-edit-project-labels">User</label>
@@ -109,43 +166,55 @@ function AddProject({ show, onClose }) {
                 onChange={handleDropDownChange}
                 options={userLilst}
               />
+              {values.assign_user.length === 0 && (
+                <p className="add-project-err-msg">User can't be empty</p>
+              )}
             </div>
             <div className="col-sm-6 col-lg-3">
               <label className="add-edit-project-labels">Start Date</label>
-
               <DatePicker
                 className="add-edit-project-inputs"
                 name="start_date"
                 suffixIcon={calanderimg}
                 onChange={(date, dateString) => {
-                  setValues({
-                    ...values,
-                    start_date: dateString,
-                  });
+                  onDateChange(date, dateString, "start_date");
                 }}
               />
+              <p className="add-project-err-msg">{errors.start_date_Err}</p>
             </div>
             <div className="col-sm-6 col-lg-3">
               <label className="add-edit-project-labels">End Date</label>
               <DatePicker
                 className="add-edit-project-inputs"
                 suffixIcon={calanderimg}
+                name="end_date"
                 onChange={(date, dateString) => {
-                  setValues({
-                    ...values,
-                    end_date: dateString,
-                  });
+                  onDateChange(date, dateString, "end_date");
                 }}
               />
+              <p className="add-project-err-msg">{errors.end_date_Err}</p>
             </div>
           </div>
           <label className="add-edit-project-labels mt-3">
             Project overview
           </label>
           <TextEditor values={values} setValues={setValues} />
+          {values.project_overview === 0 && (
+            <p className="add-project-err-msg">this field can not be empty</p>
+          )}
           <div className="d-flex mt-3 justify-content-center">
             <div className="p-2">
-              <button className="add-edit-project-submit-btn" onClick={onSubmitValue}>
+              <button
+                className={
+                  EmptyfieldsCheck()
+                    ? `add-edit-project-submit-btn-disabled`
+                    : `add-edit-project-submit-btn-primary`
+                }
+                disabled={EmptyfieldsCheck()}
+                onClick={() => {
+                  onSubmitValue();
+                }}
+              >
                 Submit
               </button>
             </div>
