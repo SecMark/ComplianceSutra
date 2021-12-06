e6186ba086a77a95abcbe42967637b4a723dac09
mjatin-dev <86044141+jatin-trakiot@users.noreply.github.com>
2021-12-06 18:49:13 +0530

    Update baseurl.js

--- src/apiServices/baseurl.js
+++ src/apiServices/baseurl.js
@@ -1,11 +1,9 @@
 let url = "";
 
 if (window.location.href.includes("localhost")) {
-  url = "https://dev.compliancesutra.com/api/method/";
-} else if (window.location.href.includes("http://52.172.231.19/")) {
-  url = "https://staging.compliancesutra.com/api/method/";
+  url = "https://dev.compliancesutra.com/api/method/"; //For development
 } else {
-  url = "https://dev.compliancesutra.com/api/method/";
+  url = "https://staging.compliancesutra.com/api/method/"; // For Production
 }
 
 export const BACKEND_BASE_URL = url;
