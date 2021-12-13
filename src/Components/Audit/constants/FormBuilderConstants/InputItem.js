b5f8d444e4d5a3042a328a775a9f65d7af713897
mjatin-dev <86044141+jatin-trakiot@users.noreply.github.com>
2021-12-13 11:28:36 +0545

    Redux store

--- src/Components/Audit/constants/FormBuilderConstants/InputItem.js
+++ src/Components/Audit/constants/FormBuilderConstants/InputItem.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 export const inputItem = [
   { name: "Date Range", id: uuidv4(), label: "isDateRange", valueType: "date" },
-  { name: "Dropdown", id: uuidv4(), label: "", valueType: "text" },
+  { name: "Dropdown", id: uuidv4(), label: "", valueType: "select" },
   { name: "Radiobox", id: uuidv4(), label: "isRadio", valueType: "radio" },
   {
     name: "Checkbox",
