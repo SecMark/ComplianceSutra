8b99557d042b9c0865d87e376f120e96387f1f01
ashukumar2021 <ashuk@trakiot.in>
2021-12-13 14:59:48 +0530

    audit assignment forms

--- src/Components/Audit/components/Inputs/Input.jsx
+++ src/Components/Audit/components/Inputs/Input.jsx
@@ -35,12 +35,15 @@ export const Input = ({
               className={styles[variant]}
             >
               <option disabled value="default" selected>
-                Select
+                {placeholder}
               </option>
               {valueForDropDown?.map((element) => {
                 return (
-                  <option key={element} value={element}>
-                    {element.name}
+                  <option
+                    key={element.id || element.value || element}
+                    value={element?.value ? element?.value : element}
+                  >
+                    {element?.label || element?.name}
                   </option>
                 );
               })}
@@ -53,19 +56,36 @@ export const Input = ({
             <label className={styles[labelVariant]}>{labelText}</label>
           </div>
           <div>
-            <input
-              onChange={onChange}
-              type={type}
-              placeholder={placeholder}
-              name={name}
-              disabled={disabled}
-              max={max}
-              min={min}
-              pattern={pattern}
-              className={styles[variant]}
-              value={value}
-              id={id}
-            />
+            {type === "textarea" ? (
+              <textarea
+                onChange={onChange}
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                disabled={disabled}
+                max={max}
+                min={min}
+                pattern={pattern}
+                className={styles[variant]}
+                value={value}
+                id={id}
+              />
+            ) : (
+              <input
+                onChange={onChange}
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                disabled={disabled}
+                max={max}
+                min={min}
+                pattern={pattern}
+                className={styles[variant]}
+                value={value}
+                id={id}
+              />
+            )}
+
             {error && <p>{errorText}</p>}
           </div>
         </>
