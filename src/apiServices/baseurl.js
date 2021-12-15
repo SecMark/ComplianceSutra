96ee6ec37e475d44683f4b5d2a6485630bfa6362
ashukumar2021 <ashuk@trakiot.in>
2021-12-15 11:55:13 +0530

    dev baseurl change

--- src/apiServices/baseurl.js
+++ src/apiServices/baseurl.js
@@ -2,7 +2,7 @@ let url = "";
 
 if (window.location.href.includes("localhost")) {
   url = "https://dev.compliancesutra.com/api/method/"; //For development
-} else if (window.location.href.includes("csutraweb-dev.azurewebsites.net")) {
+} else if (window.location.href.includes("csutraapp-dev.azurewebsites.net")) {
   url = "https://dev.compliancesutra.com/api/method/"; //For development
 } else {
   url = "https://prd.compliancesutra.com/api/method/"; // For Production
